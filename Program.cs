a5cc08cfe8229eed2bf35f364c457d0571160660
MariaDimova <150257957+MariaDimova@users.noreply.github.com>
2023-11-19 18:03:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Triangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // чете от конзолата страна и височина на триъгълник и пресмята неговото лице. 
+            // формулата за лице на триъгълник: area = a * h / 2. 
+            // Закръглете резултата до 2 знака след десетичната точка използвайки Math.Round(area, 2).
+
+            double a = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double area = Math.Round(a * h / 2, 2);
+
+            Console.WriteLine("Triangle area= " + area);
+
+
+        }
+    }
+}
